5548c0f3532120d1e00b4550f7266d36ced02873
Johnny batata <johnny.arguello007@gmail.com>
2021-09-16 23:52:27 -0300

    Updates

---
+++ precache-manifest.31759414ddf2c5608b08662006b6bef6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7edc9435adde091ad75b22f8eb9079d6",
+    "url": "/meu-portifolio/index.html"
+  },
+  {
+    "revision": "c68bf83d8d83520130c5",
+    "url": "/meu-portifolio/static/css/main.7c8f9157.chunk.css"
+  },
+  {
+    "revision": "d0254996d0459a8e37eb",
+    "url": "/meu-portifolio/static/js/2.92b5e55d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/meu-portifolio/static/js/2.92b5e55d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c68bf83d8d83520130c5",
+    "url": "/meu-portifolio/static/js/main.bc35df44.chunk.js"
+  },
+  {
+    "revision": "f1e2f2ab12960d9fb270",
+    "url": "/meu-portifolio/static/js/runtime-main.401daeb8.js"
+  },
+  {
+    "revision": "2db5b290d8e5619101092cf0b6340459",
+    "url": "/meu-portifolio/static/media/photo.2db5b290.jpeg"
+  },
+  {
+    "revision": "570c6b6424223c3018ca56922247badc",
+    "url": "/meu-portifolio/static/media/recipes.570c6b64.png"
+  }
+]);
