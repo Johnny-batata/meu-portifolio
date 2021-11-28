be22c5a41c29a6c9307db83ba0805580ac895804
jaarguello <johnny.arguello007@gmail.com>
2021-11-28 18:07:52 -0300

    adds to do list project

--- src/data.js
+++ src/data.js
@@ -2,6 +2,12 @@ import recipes from './imgs/recipes.png';
 
 const data = [
   {
+    name: 'To do list',
+    img: 'https://www.marketing91.com/wp-content/uploads/2020/03/To-do-List-scaled.jpg',
+    linkSite: 'https://johnny-batata.github.io/Ebytr-frontend/',
+    linkRepositorio: 'https://github.com/Johnny-batata/Ebytr-frontend',
+  },
+  {
     name: 'Trivia',
     img: 'https://i.pinimg.com/originals/8d/63/98/8d639893341d55296a4a6665c34abbd6.jpg',
     linkSite: 'https://johnny-batata.github.io/portif-lio---projeto-trivia/',
