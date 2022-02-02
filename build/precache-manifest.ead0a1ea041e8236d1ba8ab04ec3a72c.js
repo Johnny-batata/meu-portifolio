5c2f5c5e766c3787f7eb383423f9b7229c4f31a1
jaarguello <johnny.arguello007@gmail.com>
2022-02-02 19:00:43 -0300

    deploy

--- build/precache-manifest.ead0a1ea041e8236d1ba8ab04ec3a72c.js
+++ build/precache-manifest.ead0a1ea041e8236d1ba8ab04ec3a72c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b7feb95d0aaa09a20f629eae0953f50a",
+    "revision": "6acf125ef7e8d6ffaa58975873f1b081",
     "url": "/meu-portifolio/index.html"
   },
   {
-    "revision": "9f7504f0f2b58af62c7b",
-    "url": "/meu-portifolio/static/css/main.7c8f9157.chunk.css"
+    "revision": "7165302823b80b9210f0",
+    "url": "/meu-portifolio/static/css/main.857f064b.chunk.css"
   },
   {
     "revision": "be65883767fe78c6f7d5",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/meu-portifolio/static/js/2.8d494905.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9f7504f0f2b58af62c7b",
-    "url": "/meu-portifolio/static/js/main.15155839.chunk.js"
+    "revision": "7165302823b80b9210f0",
+    "url": "/meu-portifolio/static/js/main.a11096a0.chunk.js"
   },
   {
     "revision": "f1e2f2ab12960d9fb270",
